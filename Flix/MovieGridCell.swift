31fc797250766970a5aaf006188a6c33d9adcbd0
Rama Deshpande <rama@deshpandez.com>
2022-02-01 21:14:04 -0500

    updated unit 2
    updated unit 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Rama Deshpande on 2/1/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
